e1331b06ab0521d279b33b5d5727d3235bbc84a9
Caleb Campbell <calebcampbellcrm@gmail.com>
2022-12-07 15:15:50 -0700

    wip

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -7,16 +7,20 @@ export default function Timer(props) {
   const {x, y, name, duration} = props
   return (
     <>
-    <Draggable>
+    {/* <Draggable> */}
+    <div className='timer-wrap'>
+
     <Toolbar  />
     <div
     style={{
       left: x - 5 + 'px',
       top: y - 5 + 'px'
     }}
-    className='timer'>{name}
+    className='timer'
+    >{name}
     </div>
-    </Draggable>
+    </div>
+    {/* </Draggable> */}
     </>
   )
 }
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -12,21 +12,3 @@ export default function Toolbar(props) {
   </div>
   )
 }
-
-{/* <div className='font-size-menu'>
-<label>Font Size: {textSize}em
-<button onClick={changeFontSize} id='+' className='font-button'>+</button>
-<button onClick={changeFontSize} id='-' className='font-button'>-</button>
-</label>
-</div>
-
-<div className='font-size-menu'>
-<label>Text Color: {textColor}
-<select value={textColor}>
-  <option value={'white'}>White</option>
-  <option value={'red'}>Red</option>
-  <option value={'blue'}>Blue</option>
-  <option value={'green'}>Green</option>
-</select>
-</label>
-</div> */}
